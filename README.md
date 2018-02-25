e79ddf74f19ca6b56eba16d72dc6ca70bf792c10
Rishad Yamnoor <EvolOfThings@users.noreply.github.com>
2018-02-25 20:58:47 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Regulatory-News-App
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Regulatory News App</h1>
+      </div>
+    )
+  }
+}
+
+
+export default App;
